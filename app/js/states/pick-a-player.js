5083a849622628e5c6da86a03f6bf05b52ef81b7
monkeysppp <git@monkeysplayingpingpong.co.uk>
2017-06-12 23:07:41 +0100

    pick-a-player state

--- app/js/states/pick-a-player.js
+++ app/js/states/pick-a-player.js
@@ -1,8 +1,8 @@
 
 'use strict';
 
-// const electron = require('electron');
-// const ipc = electron.ipcRenderer;
+const electron = require('electron');
+const ipc = electron.ipcRenderer;
 
 const state = document.querySelector('.pick-a-player');
 const debug = require('../debug.js');
@@ -18,20 +18,171 @@ function init(stateManager) {
   }
 
   module.exports.internal.stateManager = stateManager;
+
+  module.exports.internal.playerAddButton = document.getElementById('button_pick-a-player_add');
+  module.exports.internal.playerName = document.getElementById('input_pick-a-player');
+  module.exports.internal.playerList = document.getElementById('pick-a-player_list');
+  module.exports.internal.breadcrumb = document.getElementById('pick-a-player_breadcrumbs');
+
+  module.exports.internal.playerAddButton.onclick = module.exports.internal.playerAddOnClick;
+  module.exports.internal.playerName.oninput = module.exports.internal.playerNameOnInput;
+}
+
+/**
+ * playerAddOnClick -A click handler for when the "add player" button is clicked.  This only
+ * acts if the player name text value contains more than 0 characters.  The team data is saved
+ * and the player list updated when this is clicked
+ *
+ * @private
+ */
+function playerAddOnClick() {
+  if (module.exports.internal.playerName.value.length > 0) {
+    let player = {
+      id: module.exports.internal.dataObj.seasons[module.exports.internal.seasonId].players.length + 1,
+      name: module.exports.internal.playerName.value
+    };
+    module.exports.internal.dataObj.seasons[module.exports.internal.seasonId].players.push(player);
+
+    debug('adding player ' + JSON.stringify(player));
+    ipc.send('save-team-data', module.exports.internal.filename, module.exports.internal.dataObj);
+  }
+}
+
+/**
+ * playerNameOnInput - An on-input handler for the player name text field.  This greys out the
+ * "add" button when there is no text in the player name.
+ *
+ * @private
+ */
+function playerNameOnInput() {
+  if (module.exports.internal.playerName.value.length === 0) {
+    module.exports.internal.playerAddButton.className = 'button new-item-button-disabled';
+  } else {
+    module.exports.internal.playerAddButton.className = 'button new-item-button';
+  }
+}
+
+/**
+ * teamDataSavedListener - React to team data being saved, by reloading team data
+ *
+ * @private
+ */
+function teamDataSavedListener() {
+  debug('team data saved, reloading team data');
+  ipc.send('get-team-data');
+}
+
+/**
+ * teamPlayerStoredListener - Reacts to the player selector being stored, then loads the player stats state.
+ *
+ * @private
+ */
+function teamPlayerStoredListener() {
+  module.exports.internal.stateManager.showState('pick-a-player', 'player-stats');
+}
+
+/**
+ * returnTeamDataListener - React to a request to get the known team data.  Populate the player list.
+ * If the number of players is 6 or more then enable the done button.
+ *
+ * @param  {object} event    IPC Event
+ * @param  {string} filename the filename that was loaded
+ * @param  {object} dataObj  the team data
+ * @param  {number} seasonId the currently selected season
+ *
+ * @private
+ */
+
+function returnTeamDataListener(event, filename, dataObj, seasonId) {
+  debug('team data loaded - seasonId ' + seasonId);
+  module.exports.internal.dataObj = dataObj;
+  module.exports.internal.filename = filename;
+  module.exports.internal.seasonId = seasonId;
+
+  // Clean up the input text box
+  module.exports.internal.playerName.value = '';
+  playerNameOnInput();
+
+  // generate the breadcrumb
+  module.exports.internal.generateBreadcrumb();
+
+  // Clean up the list div
+  let clonePlayerList = module.exports.internal.playerList.cloneNode(false);
+  module.exports.internal.playerList.parentNode.replaceChild(clonePlayerList, module.exports.internal.playerList);
+  module.exports.internal.playerList = clonePlayerList;
+
+  if (dataObj.seasons[seasonId].players) {
+    dataObj.seasons[seasonId].players.forEach((player) => {
+      let span = document.createElement('span');
+      span.innerHTML = player.name;
+      span.className = 'list-item';
+      span.onclick = () => {ipc.send('store-team-player', player.id);};
+      module.exports.internal.playerList.appendChild(span);
+    });
+  }
+
+  // Scroll to the bottom of the list
+  module.exports.internal.playerList.scrollTop = module.exports.internal.playerList.scrollHeight;
+}
+
+/**
+ * generateBreadcrumb - Generate the breadcrumb for this page:
+ *  Home > $TeamName > $SeasonName
+ *
+ * Home links back to pick a team.
+ * $TeamName links back to pick a season
+ *
+ * @private
+ */
+function generateBreadcrumb() {
+  let cloneBreadcrumb = module.exports.internal.breadcrumb.cloneNode(false);
+  module.exports.internal.breadcrumb.parentNode.replaceChild(cloneBreadcrumb, module.exports.internal.breadcrumb);
+  module.exports.internal.breadcrumb = cloneBreadcrumb;
+
+  let spanHome = document.createElement('span');
+  spanHome.innerHTML = 'Home';
+  spanHome.className = 'link';
+  spanHome.onclick = () => {module.exports.internal.stateManager.showState('pick-a-player', 'pick-a-team');};
+  module.exports.internal.breadcrumb.appendChild(spanHome);
+
+  let spanSep1 = document.createElement('span');
+  spanSep1.innerHTML = '&nbsp;&gt;&nbsp;';
+  module.exports.internal.breadcrumb.appendChild(spanSep1);
+
+  let spanTeam = document.createElement('span');
+  spanTeam.innerHTML = module.exports.internal.dataObj.name;
+  spanTeam.className = 'link';
+  spanTeam.onclick = () => {module.exports.internal.stateManager.showState('pick-a-player', 'pick-a-season');};
+  module.exports.internal.breadcrumb.appendChild(spanTeam);
+
+  let spanSep2 = document.createElement('span');
+  spanSep2.innerHTML = '&nbsp;&gt;&nbsp;';
+  module.exports.internal.breadcrumb.appendChild(spanSep2);
+
+  let spanSeason = document.createElement('span');
+  spanSeason.innerHTML = module.exports.internal.dataObj.seasons[module.exports.internal.seasonId].name;
+  module.exports.internal.breadcrumb.appendChild(spanSeason);
 }
 
 /**
  * attach - Set up any event handlers
  */
 function attach() {
-  debug('attaching pick-a-player');
+  debug('attaching add-first-squad');
+  ipc.on('team-data-saved', module.exports.internal.teamDataSavedListener);
+  ipc.on('return-team-data', module.exports.internal.returnTeamDataListener);
+  ipc.on('team-player-stored', module.exports.internal.teamPlayerStoredListener);
+  ipc.send('get-team-data');
 }
 
 /**
  * detach - Clean up any event handlers
  */
 function detach() {
-  debug('attaching pick-a-player');
+  debug('detaching add-first-squad');
+  ipc.removeListener('team-data-saved', module.exports.internal.teamDataSavedListener);
+  ipc.removeListener('return-team-data', module.exports.internal.returnTeamDataListener);
+  ipc.removeListener('team-player-stored', module.exports.internal.teamPlayerStoredListener);
 }
 
 module.exports = {
@@ -41,6 +192,19 @@ module.exports = {
   attach: attach,
   detach: detach,
   internal: {
+    teamDataSavedListener: teamDataSavedListener,
+    returnTeamDataListener: returnTeamDataListener,
+    teamPlayerStoredListener: teamPlayerStoredListener,
+    playerAddOnClick: playerAddOnClick,
+    playerNameOnInput: playerNameOnInput,
+    generateBreadcrumb: generateBreadcrumb,
     stateManager: undefined,
+    playerAddButton: undefined,
+    playerName: undefined,
+    playerList: undefined,
+    breadcrumb: undefined,
+    filename: undefined,
+    dataObj: undefined,
+    seasonId: undefined
   }
 };
---
+++ test/app/js/states/pick-a-player-test.js
@@ -0,0 +1,462 @@
+'use strict';
+
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+const proxyquire = require('proxyquire').noCallThru();
+const jsdomGlobal = require('jsdom-global');
+const fs = require('fs');
+const path = require('path');
+
+describe('app/js/pick-a-player', () => {
+  let jsdomCleanup;
+
+  let pickAPlayer;
+
+  let ipcRendererSendStub;
+  let ipcRendererOnStub;
+  let ipcRendererRemoveListenerStub;
+
+  beforeEach(function (done) {
+    this.timeout(10000);
+    jsdomCleanup = jsdomGlobal();
+
+    fs.readFile(path.join(__dirname, '..', '..', '..', '..', 'app', 'index.html'), {encodig: 'utf8'}, (err, index) => {
+      if (err) {
+        throw err;
+      }
+      document.body.innerHTML = index;
+      done();
+    });
+
+    ipcRendererSendStub = sinon.stub();
+    ipcRendererOnStub = sinon.stub();
+    ipcRendererRemoveListenerStub = sinon.stub();
+    pickAPlayer = proxyquire('../../../../app/js/states/pick-a-player.js',
+      {
+        electron: {
+          ipcRenderer: {
+            send: ipcRendererSendStub,
+            on: ipcRendererOnStub,
+            removeListener: ipcRendererRemoveListenerStub
+          }
+        }
+      }
+    );
+
+    pickAPlayer.internal.stateManager = undefined;
+    pickAPlayer.internal.playerAddButton = undefined;
+    pickAPlayer.internal.playerName = undefined;
+    pickAPlayer.internal.playerList = undefined;
+    pickAPlayer.internal.breadcrumb = undefined;
+    pickAPlayer.internal.filename = undefined;
+    pickAPlayer.internal.dataObj = undefined;
+  });
+
+  afterEach(() => {
+    jsdomCleanup();
+  });
+
+  it('returns a state handler', () => {
+    expect(typeof pickAPlayer.name).to.equal('string');
+    expect(typeof pickAPlayer.state).to.equal('object');
+    expect(typeof pickAPlayer.attach).to.equal('function');
+    expect(typeof pickAPlayer.detach).to.equal('function');
+    expect(typeof pickAPlayer.init).to.equal('function');
+  });
+
+  describe('#attach', () => {
+    it('registers for team-data-saved', () => {
+      pickAPlayer.attach();
+      expect(ipcRendererOnStub).to.be.calledWith('team-data-saved', pickAPlayer.internal.teamDataSavedListener);
+    });
+
+    it('registers for return-team-data', () => {
+      pickAPlayer.attach();
+      expect(ipcRendererOnStub).to.be.calledWith('return-team-data', pickAPlayer.internal.returnTeamDataListener);
+    });
+
+    it('registers for team-player-stored', () => {
+      pickAPlayer.attach();
+      expect(ipcRendererOnStub).to.be.calledWith('team-player-stored', pickAPlayer.internal.teamPlayerStoredListener);
+    });
+
+    it('sends a get-team-data event', () => {
+      pickAPlayer.attach();
+      expect(ipcRendererSendStub).to.be.calledOnce;
+      expect(ipcRendererSendStub).to.be.calledWith('get-team-data');
+    });
+  });
+
+  describe('#detach', () => {
+    it('deregisters for team-data-saved', () => {
+      pickAPlayer.detach();
+      expect(ipcRendererRemoveListenerStub).to.be.calledWith('team-data-saved', pickAPlayer.internal.teamDataSavedListener);
+    });
+
+    it('deregisters for return-team-data', () => {
+      pickAPlayer.detach();
+      expect(ipcRendererRemoveListenerStub).to.be.calledWith('return-team-data', pickAPlayer.internal.returnTeamDataListener);
+    });
+
+    it('deregisters for team-player-stored', () => {
+      pickAPlayer.detach();
+      expect(ipcRendererRemoveListenerStub).to.be.calledWith('team-player-stored', pickAPlayer.internal.teamPlayerStoredListener);
+    });
+  });
+
+  describe('#init', () => {
+    context('called without a state-manager', () => {
+      it('throws an error', () => {
+        expect(() => {pickAPlayer.init();}).to.throw('no state-manager given');
+      });
+    });
+
+    context('called with a state-manager', () => {
+      let stateManagerStub;
+
+      beforeEach(() => {
+        stateManagerStub = {};
+        pickAPlayer.init(stateManagerStub);
+      });
+
+      it('saves the state manager', () => {
+        expect(pickAPlayer.internal.stateManager).to.equal(stateManagerStub);
+      });
+
+      it('finds the playerAdd button', () => {
+        expect(pickAPlayer.internal.playerAddButton).to.equal(document.getElementById('button_pick-a-player_add'));
+      });
+
+      it('finds the playerName textbox', () => {
+        expect(pickAPlayer.internal.playerName).to.equal(document.getElementById('input_pick-a-player'));
+      });
+
+      it('finds the playerList div', () => {
+        expect(pickAPlayer.internal.playerList).to.equal(document.getElementById('pick-a-player_list'));
+      });
+
+      it('finds the breadcrumb div', () => {
+        expect(pickAPlayer.internal.breadcrumb).to.equal(document.getElementById('pick-a-player_breadcrumbs'));
+      });
+
+      it('sets the playerAdd onclick listener for the button', () => {
+        expect(pickAPlayer.internal.playerAddButton.onclick).to.equal(pickAPlayer.internal.playerAddOnClick);
+      });
+
+      it('sets the oninput listener for the input', () => {
+        expect(pickAPlayer.internal.playerName.oninput).to.equal(pickAPlayer.internal.playerNameOnInput);
+      });
+    });
+  });
+
+  describe('#generateBreadcrumb', () => {
+    let stateManagerStub;
+    let showStateStub;
+    let dataObj;
+    let breadcrumbParts;
+
+    beforeEach(() => {
+      showStateStub = sinon.stub();
+      stateManagerStub = {
+        showState: showStateStub
+      };
+      dataObj = {
+        name: 'team1',
+        seasons: [
+          {
+            name: '2010/2011',
+            players: [
+              {id: 1, name: 'Alice Alison'},
+              {id: 2, name: 'Bob Roberts'},
+              {id: 3, name: 'Charlie Charlson'},
+              {id: 4, name: 'Debbie Davis'},
+              {id: 5, name: 'Emma Emerton'},
+              {id: 6, name: 'Freda Ferguson'},
+            ]
+          },
+          {
+            name: '2011/2012'
+          }
+        ]
+      };
+      pickAPlayer.internal.dataObj = dataObj;
+      pickAPlayer.internal.seasonId = 0;
+      pickAPlayer.init(stateManagerStub);
+      pickAPlayer.internal.generateBreadcrumb();
+      breadcrumbParts = document.getElementById('pick-a-player_breadcrumbs').childNodes;
+    });
+
+    it('generates a breadcrumb', () => {
+      expect(breadcrumbParts[0].innerHTML).to.equal('Home');
+      expect(breadcrumbParts[2].innerHTML).to.equal(dataObj.name);
+      expect(breadcrumbParts[4].innerHTML).to.equal(dataObj.seasons[0].name);
+    });
+
+    it('cleans out the old breadcrumb on each call', () => {
+      pickAPlayer.internal.generateBreadcrumb();
+      pickAPlayer.internal.generateBreadcrumb();
+      expect(breadcrumbParts.length).to.equal(5);
+    });
+
+    it('makes the home button show "pick-a-team"', () => {
+      expect(typeof breadcrumbParts[0].onclick).to.equal('function');
+      breadcrumbParts[0].onclick();
+      expect(showStateStub).to.be.calledWith('pick-a-player', 'pick-a-team');
+    });
+
+    it('makes the team button show "pick-a-season"', () => {
+      expect(typeof breadcrumbParts[0].onclick).to.equal('function');
+      breadcrumbParts[2].onclick();
+      expect(showStateStub).to.be.calledWith('pick-a-player', 'pick-a-season');
+    });
+  });
+
+  describe('#playerAddOnClick', () => {
+    let startingDataObj;
+    let expectedDataObj;
+
+    beforeEach(() => {
+      startingDataObj = {
+        name: 'team1',
+        seasons: [
+          {
+            name: '2010/2011',
+            players: [
+              {id: 1, name: 'abc'}
+            ]
+          }
+        ]
+      };
+      expectedDataObj = {
+        name: 'team1',
+        seasons: [
+          {
+            name: '2010/2011',
+            players: [
+              {id: 1, name: 'abc'},
+              {id: 2, name: 'xyz'}
+            ]
+          }
+        ]
+      };
+      pickAPlayer.init({});
+      pickAPlayer.internal.dataObj = startingDataObj;
+      pickAPlayer.internal.filename = 'someFileName';
+      pickAPlayer.internal.seasonId = 0;
+    });
+
+    context('when input is zero length', () => {
+      beforeEach(() => {
+        pickAPlayer.internal.playerName.value = '';
+        pickAPlayer.internal.playerAddOnClick();
+      });
+
+      it('disables the button', () => {
+        expect(ipcRendererSendStub).to.not.be.called;
+      });
+    });
+
+    context('when input is longer than zero length', () => {
+      beforeEach(() => {
+        pickAPlayer.internal.playerName.value = 'xyz';
+        pickAPlayer.internal.playerAddOnClick();
+      });
+
+      it('adds the player to the team data', () => {
+        expect(ipcRendererSendStub).to.be.calledWith('save-team-data', 'someFileName', expectedDataObj);
+      });
+    });
+  });
+
+  describe('#playerNameOnInput', () => {
+    beforeEach(() => {
+      pickAPlayer.init({});
+      pickAPlayer.internal.playerAddButton.className = 'null';
+    });
+
+    context('when input is zero length', () => {
+      beforeEach(() => {
+        pickAPlayer.internal.playerName.value = '';
+        pickAPlayer.internal.playerNameOnInput();
+      });
+
+      it('sets the button class to disabled', () => {
+        expect(pickAPlayer.internal.playerAddButton.className).to.equal('button new-item-button-disabled');
+      });
+    });
+
+    context('when input is longer than zero length', () => {
+      beforeEach(() => {
+        pickAPlayer.internal.playerName.value = 'abc';
+        pickAPlayer.internal.playerNameOnInput();
+      });
+
+      it('sets the button class to enabled', () => {
+        expect(pickAPlayer.internal.playerAddButton.className).to.equal('button new-item-button');
+      });
+    });
+  });
+
+  describe('#returnTeamDataListener', () => {
+    let stateManagerStub;
+    let showStateStub;
+    let generateBreadcrumbStub;
+    let dataObj = {
+      name: 'team1',
+      seasons: [
+        {
+          name: '2011/2012'
+        },
+        {
+          name: '2010/2011',
+          players: [
+            {id: 1, name: 'Alice Alison'},
+            {id: 2, name: 'Bob Roberts'},
+            {id: 3, name: 'Charlie Charlson'},
+            {id: 4, name: 'Debbie Davis'},
+            {id: 5, name: 'Emma Emerton'},
+            {id: 6, name: 'Freda Ferguson'},
+          ]
+        }
+      ]
+    };
+
+    beforeEach(() => {
+      showStateStub = sinon.stub();
+      stateManagerStub = {
+        showState: showStateStub
+      };
+      generateBreadcrumbStub = sinon.stub(pickAPlayer.internal, 'generateBreadcrumb');
+      pickAPlayer.init(stateManagerStub);
+      pickAPlayer.internal.dataObj = {};
+      pickAPlayer.internal.filename = 'foo';
+      pickAPlayer.internal.seasonId = 0;
+    });
+
+    afterEach(() => {
+      generateBreadcrumbStub.restore();
+    });
+
+    it('locally stores the filename', () => {
+      pickAPlayer.internal.returnTeamDataListener(undefined, 'someFileName', dataObj, 0);
+      expect(pickAPlayer.internal.filename).to.equal('someFileName');
+    });
+
+    it('locally stores the team data', () => {
+      pickAPlayer.internal.returnTeamDataListener(undefined, undefined, dataObj, 0);
+      expect(pickAPlayer.internal.dataObj).to.deep.equal(dataObj);
+    });
+
+    it('locally stores the seasonId', () => {
+      pickAPlayer.internal.returnTeamDataListener(undefined, 'someFileName', dataObj, 1);
+      expect(pickAPlayer.internal.seasonId).to.equal(1);
+    });
+
+    it('clears the curent playerName input and add button', () => {
+      pickAPlayer.internal.playerName.value = 'sometext';
+      pickAPlayer.internal.returnTeamDataListener(undefined, undefined, dataObj, 0);
+      expect(pickAPlayer.internal.playerName.value).to.equal('');
+      expect(pickAPlayer.internal.playerAddButton.className).to.equal('button new-item-button-disabled');
+    });
+
+    it('calls to generate the breadcrumb', () => {
+      pickAPlayer.internal.returnTeamDataListener(undefined, undefined, dataObj, 0);
+      expect(generateBreadcrumbStub).to.be.calledOnce;
+    });
+
+    context('the list items', () => {
+      it('have an onclick that stored the player id', () => {
+        pickAPlayer.internal.returnTeamDataListener(undefined, undefined, dataObj, 1);
+
+        let listItems = pickAPlayer.internal.playerList.getElementsByClassName('list-item');
+
+        expect(listItems[0].innerHTML).to.equal(dataObj.seasons[1].players[0].name);
+        expect(typeof listItems[0].onclick).to.equal('function');
+        listItems[0].onclick();
+        expect(ipcRendererSendStub).to.be.calledWith('store-team-player', 1);
+
+        expect(listItems[1].innerHTML).to.equal(dataObj.seasons[1].players[1].name);
+        expect(typeof listItems[1].onclick).to.equal('function');
+        listItems[1].onclick();
+        expect(ipcRendererSendStub).to.be.calledWith('store-team-player', 2);
+
+        expect(listItems[2].innerHTML).to.equal(dataObj.seasons[1].players[2].name);
+        expect(typeof listItems[2].onclick).to.equal('function');
+        listItems[2].onclick();
+        expect(ipcRendererSendStub).to.be.calledWith('store-team-player', 3);
+
+        expect(listItems[3].innerHTML).to.equal(dataObj.seasons[1].players[3].name);
+        expect(typeof listItems[3].onclick).to.equal('function');
+        listItems[3].onclick();
+        expect(ipcRendererSendStub).to.be.calledWith('store-team-player', 4);
+
+        expect(listItems[4].innerHTML).to.equal(dataObj.seasons[1].players[4].name);
+        expect(typeof listItems[4].onclick).to.equal('function');
+        listItems[4].onclick();
+        expect(ipcRendererSendStub).to.be.calledWith('store-team-player', 5);
+
+        expect(listItems[5].innerHTML).to.equal(dataObj.seasons[1].players[5].name);
+        expect(typeof listItems[5].onclick).to.equal('function');
+        listItems[5].onclick();
+        expect(ipcRendererSendStub).to.be.calledWith('store-team-player', 6);
+      });
+
+      it('get cleaned out on each load call', () => {
+        pickAPlayer.internal.returnTeamDataListener(undefined, undefined, dataObj, 1);
+        pickAPlayer.internal.returnTeamDataListener(undefined, undefined, dataObj, 1);
+        pickAPlayer.internal.returnTeamDataListener(undefined, undefined, dataObj, 1);
+
+        let listItems = pickAPlayer.internal.playerList.getElementsByClassName('list-item');
+        expect(pickAPlayer.internal.playerList.childNodes.length).to.equal(6);
+        expect(listItems[0].innerHTML).to.equal('Alice Alison');
+        expect(listItems[1].innerHTML).to.equal('Bob Roberts');
+        expect(listItems[2].innerHTML).to.equal('Charlie Charlson');
+        expect(listItems[3].innerHTML).to.equal('Debbie Davis');
+        expect(listItems[4].innerHTML).to.equal('Emma Emerton');
+        expect(listItems[5].innerHTML).to.equal('Freda Ferguson');
+      });
+    });
+  });
+
+  describe('#teamDataSavedListener', () => {
+    beforeEach(() => {
+      pickAPlayer.internal.dataObj = {
+        name: 'team1',
+        players: [
+          {name: 'abc'},
+          {name: 'xyz'},
+          {name: '123/456'}
+        ]
+      };
+    });
+
+    it('calls to get the team data', () => {
+      pickAPlayer.internal.teamDataSavedListener();
+      expect(ipcRendererSendStub).to.be.calledWith('get-team-data');
+    });
+  });
+
+  describe('#teamPlayerStoredListener', () => {
+    let stateManagerStub;
+    let showStateStub;
+
+    beforeEach(() => {
+      showStateStub = sinon.stub();
+      stateManagerStub = {
+        showState: showStateStub
+      };
+      pickAPlayer.init(stateManagerStub);
+    });
+
+    it('shows player-stats', () => {
+      pickAPlayer.internal.teamPlayerStoredListener();
+      expect(showStateStub).to.be.calledWith('pick-a-player', 'player-stats');
+    });
+  });
+});
